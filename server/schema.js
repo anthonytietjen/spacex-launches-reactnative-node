fd325eb807a57bef588e2d7ab63bd78fc5b4d89d
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-24 18:48:47 -0600

    Added server

---
+++ server/schema.js
@@ -0,0 +1,45 @@
+const axios = require("axios").default;
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
+} = require("graphql");
+
+const ILaunch = new GraphQLObjectType({
+  name: "ILaunch",
+  fields: () => ({
+    flight_number: {
+      type: GraphQLInt,
+    },
+    mission_name: {
+      type: GraphQLString,
+    },
+    launch_year: {
+      type: GraphQLString,
+    },
+    launch_success: {
+      type: GraphQLBoolean,
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    launches: {
+      type: new GraphQLList(ILaunch),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/launches")
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
