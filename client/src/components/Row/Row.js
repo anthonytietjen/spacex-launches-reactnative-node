c874209906b3adf49950ff21cab2f7ccf2fd7f7c
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-30 23:14:33 -0600

    Row styling

--- client/src/components/Row/Row.js
+++ client/src/components/Row/Row.js
@@ -9,23 +9,25 @@ export const Row = ({ launch, navigation, handleClick }) => {
   return (
     <TouchableOpacity onPress={onClick}>
       <View style={styles.container}>
+        <Text style={styles.title}>{launch.mission_name}</Text>
         <View style={styles.info}>
-          <Text style={styles.text}>{launch.mission_name}</Text>
-          <Text style={styles.text}>Flight #: {launch.flight_number}</Text>
-          <Text style={styles.text}>
-            {new Date(launch.launch_date_local).toLocaleDateString("en-US")}
-          </Text>
+          <View>
+            <Text style={styles.text}>Flight #: {launch.flight_number}</Text>
+            <Text style={styles.text}>
+              {new Date(launch.launch_date_local).toLocaleDateString("en-US")}
+            </Text>
+          </View>
+          {launch.launch_success && (
+            <View style={styles.success}>
+              <Text>Success</Text>
+            </View>
+          )}
+          {!launch.launch_success && (
+            <View style={styles.fail}>
+              <Text>Fail</Text>
+            </View>
+          )}
         </View>
-        {launch.launch_success && (
-          <View style={styles.success}>
-            <Text>Success</Text>
-          </View>
-        )}
-        {!launch.launch_success && (
-          <View style={styles.fail}>
-            <Text>Fail</Text>
-          </View>
-        )}
       </View>
     </TouchableOpacity>
   );
@@ -36,8 +38,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#222",
     padding: 15,
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: "column",
     borderRadius: 6,
     marginTop: 15,
     marginLeft: 15,
@@ -45,7 +46,15 @@ const styles = StyleSheet.create({
     marginBottom: 0,
   },
   info: {
+    flex: 1,
     display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 20,
+    marginBottom: 8,
   },
   text: {
     color: "#FFF",
@@ -53,9 +62,13 @@ const styles = StyleSheet.create({
   success: {
     padding: 8,
     backgroundColor: "green",
+    borderRadius: 4,
+    minWidth: 75,
   },
   fail: {
     padding: 8,
     backgroundColor: "red",
+    borderRadius: 4,
+    minWidth: 75,
   },
 });
